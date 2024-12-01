82f9db095d6e6dbd2d840cfcf3059287c8a2dda9
Jinninni040507 <moni.oliveras05@gmail.com>
2024-12-01 18:56:29 +0100

    #3 Search nurses by name done & menu routing implemented

---
+++ NurseProject_Frontend/src/app/search-nurses-by-name/search-nurses-by-name.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NURSE_USERS } from '../nurse-users';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-search-nurses-by-name',
+  templateUrl: './search-nurses-by-name.component.html',
+  styleUrl: './search-nurses-by-name.component.css',
+  imports: [CommonModule],
+})
+export class SearchNursesByNameComponent {
+  foundNurses = NURSE_USERS;
+
+  listFoundNurses(name: string = ''): void {
+    if (name.trim() !== '') {
+      this.foundNurses = NURSE_USERS.filter((nurse) =>
+        nurse.name.toLowerCase().includes(name.toLowerCase())
+      );
+    } else {
+      this.foundNurses = NURSE_USERS;
+    }
+  }
+}
