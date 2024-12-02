6d2cda30550a43c6239e78a99fa73ab7fa13ad75
Jinninni040507 <moni.oliveras05@gmail.com>
2024-12-02 22:09:11 +0100

    component for Akisha

---
+++ NurseProject_Frontend/src/app/list-all-nurses/list-all-nurses.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-all-nurses',
+  imports: [],
+  templateUrl: './list-all-nurses.component.html',
+  styleUrl: './list-all-nurses.component.css'
+})
+export class ListAllNursesComponent {
+
+}
